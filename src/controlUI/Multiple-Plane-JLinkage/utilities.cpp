c845744c8454b5e8d7d073864bf2b93fa68927c0
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2016-03-29 15:44:48 +0530

    Corrected fitPlane3D function for normalization

--- src/controlUI/Multiple-Plane-JLinkage/utilities.cpp
+++ src/controlUI/Multiple-Plane-JLinkage/utilities.cpp
@@ -54,58 +54,6 @@ int numberOfUniquePlanes(
 
 }
 
-void fitFunctionPlane(
-	const vector<Point3f> &planePoints,
-	const vector<int> planeIndices,
-	vector< vector<float> >  &planeParameters) {
-
-	vector< vector<Point3f> > planeOrderedPoints;
-	int numberOfPlanes = numberOfUniquePlanes(planeIndices);
-	int numberOfPointsInThePlane = planePoints.size();
-	vector<Point3f> pointsOfAPlane;
-	int i, j;
-
-	for (i = 0; i < numberOfPlanes; ++i) {
-		planeOrderedPoints.push_back(pointsOfAPlane);
-	}
-
-	for (i = 0; i < numberOfPointsInThePlane; ++i) {
-		planeOrderedPoints[planeIndices[i]].push_back(planePoints[i]);
-	}
-
-	float x1, x2, y1, y2, z1, z2, normN;
-	float a, b, c, d;
-	for (i = 0; i < numberOfPlanes; ++i) {
-
-		// Normal found as cross product of X2 - X1, X3 - X1
-		//for (j = 0; j < 3; ++j) {
-			x1 = planeOrderedPoints[i][1].x - planeOrderedPoints[i][0].x;
-			x2 = planeOrderedPoints[i][2].x - planeOrderedPoints[i][0].x;
-			y1 = planeOrderedPoints[i][1].y - planeOrderedPoints[i][0].y;
-			y2 = planeOrderedPoints[i][2].y - planeOrderedPoints[i][0].y;
-			z1 = planeOrderedPoints[i][1].z - planeOrderedPoints[i][0].z;
-			z2 = planeOrderedPoints[i][2].z - planeOrderedPoints[i][0].z;
-
-			// Compute a, b, c, d in a*x+b*y+c*z+d=0
-			a = y1 * z2 - z1 * y2;
-			b = z1 * x2 - x1 * z2;
-			c = x1 * y2 - y1 * x2;
-			d =  - (a * planeOrderedPoints[i][0].x ) - (b * planeOrderedPoints[i][0].y ) - (c * planeOrderedPoints[i][0].z ); 
-
-			// Normalize
-			normN = sqrt(a*a + b*b + c*c);
-			a = a/normN;
-			b = b/normN;
-			c = c/normN;
-			d = d/normN;
-		//}
-
-	}
-
-	return ;
-
-}
-
 void getPlaneParameters(
 	const vector<Point3f> &planePoints,
 	const vector<int> planeIndices,
@@ -166,12 +114,12 @@ void fitPlane3D(
 	Mat minEigVector = eigenvectors.row(2);
 	float normSum = 0.0;
 	for(j = 0; j < 3; ++j) {
-		normSum += minEigVector.at<float>(0, j);
+		normSum += (minEigVector.at<float>(0, j))*(minEigVector.at<float>(0, j));
 	}
-	Mat abc = minEigVector;
 	for(j = 0; j < 3; ++j) {
 		minEigVector.at<float>(0, j) = minEigVector.at<float>(0, j)/normSum;
 	}
+	Mat abc = minEigVector;
 	a = abc.at<float>(0, 0); b = abc.at<float>(0, 1); c = abc.at<float>(0, 2); d = 0.0;
 	d += centroidX*a; d += centroidY*b; d += centroidZ*c;
 	d = (-1.0)*d; 
